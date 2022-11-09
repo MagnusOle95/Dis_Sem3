c8cdc5a6e8477b100c93135d0ccc1ebc75fb9467
magnu <magnus.s.larsen@hotmail.com>
2022-11-09 10:09:13 +0100

    Dag 18 opgave 2 fra dag 17

---
+++ Dag_17_18_Distribueret_Databaser/src/opgave2/highTransparancy/ViaViewclass.java
@@ -0,0 +1,44 @@
+package opgave2.highTransparancy;
+import opgave2.Person;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ViaViewclass {
+    public static void main(String[] args) {
+        try {
+            ArrayList<Person> liste = new ArrayList<Person>();
+//	 læser viewet person via native SQL-Server driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connection minConnection;
+            minConnection = DriverManager.getConnection("jdbc:sqlserver://localhost\\SQLEXPRESS;" +
+                    "databaseName=ddba;user=sa;password=torben07;");
+            String sql= "select * from Person";
+            Statement stmt = minConnection.createStatement();
+            ResultSet res=stmt.executeQuery(sql);
+            while (res.next()) {
+                Person p = new Person();
+                p.setNavn(res.getString("lname"));
+                p.setFornavn(res.getString("fname"));
+                p.setKoen(res.getString("sex"));
+                p.setLoen(res.getInt("sal"));
+                p.setPostnr(res.getString("zip"));
+                liste.add(p);
+            };
+//	 udskriver indholdet af de to tabeller
+            for (int i=0;i<liste.size();i++) {
+                Person s =liste.get(i);
+                System.out.print(s.getNavn()+ "    ");
+                System.out.print(s.getFornavn() + "    ");
+                System.out.print(s.getKoen() + "     ");
+                System.out.print(s.getLoen() + "     ");
+                System.out.println(s.getPostnr());
+            }
+        }
+        catch (Exception e) {
+            System.out.println("fejl:  "+e.getMessage());
+        }
+    }
+}
